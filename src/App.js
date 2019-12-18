c211a2b6dda0ab86e505d594dcc194d1dd7ba2e3
Kim <mjkim1201@naver.com>
2019-12-18 20:33:34 +0900

    여러개 컴포넌드 return 해보기

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
+import {Person ,My} from "./Person";
 
 function App() {
   return (
     <div className="App">
       <h1>안녕하세요 react 입니다 </h1>
       <Person></Person>
+      <My></My>
+     
     </div>
   );
 }
 
 
-function Person(){
-  return <h1>첫번째 컴포넌트입니다. </h1>
-}
 
 
 export default App;
---
+++ src/Person.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+
+
+const  Person = () => { // ES6문법 
+    return <h1>첫번째 컴포넌트입니다. </h1>
+  }
+
+  const My =() => {
+      return  <h1> return 연습하기 </h1>
+  }
+
+export { Person , My}
